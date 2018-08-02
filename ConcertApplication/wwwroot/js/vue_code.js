a308772058045ea854efe85d6e493bf09ca792fb
juicyjelly74 <juicy.jelly74@gmail.com>
2018-08-02 18:48:10 +0300

    Add function to get addresses

--- ConcertApplication/wwwroot/js/vue_code.js
+++ ConcertApplication/wwwroot/js/vue_code.js
@@ -10,12 +10,19 @@ Vue.component('concert-map', {
         return {
             mapName: 'concert-map',
             markerCoordinates: [{
+                latitude: 53.9,
+                longitude: 27.5667
+            },
+            {
                 latitude: 51.501527,
                 longitude: -0.1921837
-            }, {
+            },
+
+            {
                 latitude: 51.505874,
                 longitude: -0.1838486
-            }, {
+            },
+            {
                 latitude: 51.4998973,
                 longitude: -0.202432
             }],
@@ -24,14 +31,56 @@ Vue.component('concert-map', {
             markers: [],
             addresses: [],
             searchAddressInput: '',
-            currentAddress: '',
-            currentLocation: '',
+            currentLocation: {
+                latitude: 53.9,
+                longitude: 27.5667
+            },
             addresses: []
         }
     },
     mounted() {
 
-        this.getMarkerCoordinates();
+        var fff = (response, resolve) => {
+            this.addresses = response.data;
+            resolve("Success!");
+        };
+        myPromise = new Promise((resolve, reject) => {
+            try {
+                var error;
+                axios({
+                    method: 'get',
+                    url: '/concerts/getPlaces'
+                })
+                    .then((data) => fff(data, resolve))
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            } catch (ex) {
+                console.log(ex);
+            }
+        });
+
+        myPromise.then((successMessage) => {
+            this.getMarkerCoordinates(); 
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        myPromise.then((successMessage) => {
+            this.markerCoordinates.forEach((coord) => {
+                const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+                const marker = new google.maps.Marker({
+                    position,
+                    map: this.map
+                });
+                this.markers.push(marker);
+                this.map.fitBounds(this.bounds.extend(position));
+            });
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
 
         this.bounds = new google.maps.LatLngBounds();
         const element = document.getElementById('concert-map')
@@ -40,43 +89,24 @@ Vue.component('concert-map', {
             center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude)
         }
         this.map = new google.maps.Map(element, options);
-        this.markerCoordinates.forEach((coord) => {
-            const position = new google.maps.LatLng(coord.latitude, coord.longitude);
-            const marker = new google.maps.Marker({
-                position,
-                map: this.map
-            });
-            this.markers.push(marker);
-            this.map.fitBounds(this.bounds.extend(position));
-        });
+
     },
     methods: {
-        searchLocation: function () {
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ 'address': this.searchAddressInput }, (results, status) => {
-                if (status === 'OK') {
-                    this.currentLocation.lat = results[0].geometry.location.lat();
-                    this.currentLocation.lng = results[0].geometry.location.lng();
-                }
-            });
-        },
         getMarkerCoordinates: function () {
-            this.getAddresses();
+            var that = this;
             var geocoder = new google.maps.Geocoder();
             this.addresses.forEach((address) => {
-                geocoder.geocode({ 'address': this.address }, (results, status) => {
-                    if (status === 'OK') { 
-                        this.currentAddress.lat = results[0].geometry.location.lat();
-                        this.currentAddress.lng = results[0].geometry.location.lng();
-                        this.markerCoordinates.push(currentAddress);
+                geocoder.geocode({ 'address': address }, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        that.currentLocation.latitude = results[0].geometry.location.lat();
+                        that.currentLocation.longitude = results[0].geometry.location.lng();
+                        that.markerCoordinates.push(that.currentLocation);
                     }
                 });
             });
         },
         getAddresses: function () {
-            axios
-                .get('/concerts/getPlaces')
-                .then(response => (this.addresses = response.data));
+            
         }
     },
     template: '#temp'
